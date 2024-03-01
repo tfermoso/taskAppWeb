91129b2d6f9857586a158353741b472418c21a9f
tfermoso <tomas.fermoso@gmail.com>
2024-03-01 18:35:51 +0100

    peticion fetch

---
+++ src/main/java/com/ceica/tareasweb/servlets/ApiServlet.java
@@ -0,0 +1,19 @@
+package com.ceica.tareasweb.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "apiServlet", value = "/api")
+public class ApiServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        PrintWriter out=response.getWriter();
+        out.write("hola desde la api");
+    }
+}
---
+++ src/main/webapp/assets/js/user.js
@@ -0,0 +1,24 @@
+window.onload=()=>{
+
+
+}
+
+function borrar(idtask){
+    var baseUrl = window.location.protocol + "//" + window.location.host + "/tareasweb/api";
+
+    fetch(baseUrl, {
+        method: 'GET', // Puedes cambiar este método según tu necesidad (POST, PUT, DELETE, etc.)
+        headers: {
+            'Content-Type': 'application/json', // Puedes ajustar el tipo de contenido según tu necesidad
+        },
+        // Puedes agregar otros parámetros como body si estás enviando datos
+    })
+        .then(response => response.json()) // Procesamos la respuesta como JSON
+        .then(data => {
+            console.log(data); // Aquí puedes hacer lo que quieras con los datos
+        })
+        .catch(error => {
+            console.error('Error:', error); // Manejamos los errores
+        });
+
+}
